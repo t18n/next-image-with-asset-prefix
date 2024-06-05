80cdca39ea70190a8c607550da840e42fb8b67d6
t18n <me@t18n.com>
2024-06-05 23:39:04 +0200

    docs: add more docs

--- nextImageLoader.mjs
+++ nextImageLoader.mjs
@@ -1,4 +1,7 @@
-// A custom loader is needed to handle the CDN URLs
+/*
+  * A custom loader is needed to handle the CDN URLs
+  * You don't need to prepend the CDN URL in the src attribute, since the custom loader respects the assetPrefix (weird, but it works)
+ */
 export default function cdnLoader({ src, width, quality }) {
   return `${src}?w=${width}&q=${quality || 75}`
 }
