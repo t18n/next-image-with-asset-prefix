11c75dbf094d45fcc2f78bf0d863029a1baff0ba
t18n <me@t18n.com>
2024-06-05 23:36:08 +0200

    feat: add asset prefix

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  assetPrefix: process.env.NEXT_PUBLIC_ASSET_PREFIX, // Set your CDN URL or prefix here
   images: {
     loader: 'custom',
     loaderFile: './nextImageLoader.mjs',
